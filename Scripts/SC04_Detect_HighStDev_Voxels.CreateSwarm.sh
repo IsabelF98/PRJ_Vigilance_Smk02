50476e455c38acb4dcde6b37a1ca888e63d3d10a
Isabel Fernandez <izyf98@gmail.com>
2020-11-30 10:48:29 -0500

    added number of subjects count to script

--- Scripts/SC04_Detect_HighStDev_Voxels.CreateSwarm.sh
+++ Scripts/SC04_Detect_HighStDev_Voxels.CreateSwarm.sh
@@ -1,5 +1,5 @@
 # 11/23/2020 - Isabel Fernandez
-#
+
 set -e
 
 ORIG_DATA_DIR='/data/SFIM_Vigilance/Data/DSET02/'
@@ -7,6 +7,7 @@ subjects=(`ls ${ORIG_DATA_DIR} | tr -s '\n' ' '`)
 subjects=("${subjects[@]/'README'}") # The subject directory contains a README file. This is not a subject ID.
 subjects=("${subjects[@]/'dataset_description.json'}") # The subject directory contains a json file. This is not a subject ID.
 subjects=("${subjects[@]/'sub-S21'}") # This subject had bad motion and will not be used.
+num_subjects=`echo "${#subjects[@]} -1" | bc -l`
 echo "Subjects: ${subjects[@]}"
 
 # Create log directory if needed
@@ -16,12 +17,12 @@ fi
 
 # Write top comment in Swarm file 
 echo "#Creation Date: `date`" > ./SC04_Detect_HighStDev_Voxels.SWARM.sh
-echo "#swarm -f ./SC04_Detect_HighStDev_Voxels.SWARM.sh -g 32 -t 32 --partition quick,norm --logdir ./SC04_Detect_HighStDev_Voxels.logs" >> ./SC04_Detect_HighStDev_Voxels.SWARM.sh
+echo "#swarm -f ./SC04_Detect_HighStDev_Voxels.SWARM.sh -g 32 -t 32 --partition quick,norm --module afni --logdir ./SC04_Detect_HighStDev_Voxels.logs --sbatch \"--export AFNI_COMPRESSOR=GZIP\"" >> ./SC04_Detect_HighStDev_Voxels.SWARM.sh
 
 # Write one entry per subject in Swarm file
 for i in `seq 0 1 ${num_subjects}`
 do
-   if [ ! -z ${subjects[i]} ]; then 
+   if [ ! -z ${subjects[i]} ]; then
       echo "export SBJ=${subjects[i]}; sh ./SC04_Detect_HighStDev_Voxels.sh" >> ./SC04_Detect_HighStDev_Voxels.SWARM.sh
    fi
 done
