3c061fbdc770b2450d2a39ddc265b29ad77c1fec
Isabel Fernandez <izyf98@gmail.com>
2020-11-30 16:06:53 -0500

    updated scripts for data set 2

--- Scripts/SC07_AdaptAtlasToSubject.CreateSwarm.sh
+++ Scripts/SC07_AdaptAtlasToSubject.CreateSwarm.sh
@@ -1,4 +1,4 @@
-# Oct/28/2020 - Javier Gonzalez-Castillo
+# 11/30/2020 - Isabel Fernandez
 #
 # STEPS
 #
@@ -8,9 +8,11 @@ set -e
 
 NROIs=200
 NumROIsID=`printf %04d ${NROIS}`
-ORIG_DATA_DIR='/data/SFIM_Vigilance/Data/DSET01/'
+ORIG_DATA_DIR='/data/SFIM_Vigilance/Data/DSET02/'
 subjects=(`ls ${ORIG_DATA_DIR} | tr -s '\n' ' '`)
-subjects=("${subjects[@]/'README'}")   # The subject directory contains a README file. This is not a subject ID.
+subjects=("${subjects[@]/'README'}") # The subject directory contains a README file. This is not a subject ID.
+subjects=("${subjects[@]/'dataset_description.json'}") # The subject directory contains a json file. This is not a subject ID.
+subjects=("${subjects[@]/'sub-S21'}") # This subject had bad motion and will not be used.
 num_subjects=`echo "${#subjects[@]} -1" | bc -l`
 echo "Subjects: ${subjects[@]}"
 
@@ -21,7 +23,7 @@ fi
 
 # Write top comment in Swarm file 
 echo "#Creation Date: `date`" > ./SC07_AdaptAtlasToSubject.SWARM.sh
-echo "#swarm -f ./SC07_AdaptAtlasToSubject.SWARM.sh -g 32 -t 32 --partition quick,norm --logdir ./SC07_AdaptAtlasToSubject.logs" >> ./SC07_AdaptAtlasToSubject.SWARM.sh
+echo "#swarm -f ./SC07_AdaptAtlasToSubject.SWARM.sh -g 32 -t 32 --partition quick,norm --module afni --logdir ./SC07_AdaptAtlasToSubject.logs --sbatch \"--export AFNI_COMPRESSOR=GZIP\"" >> ./SC07_AdaptAtlasToSubject.SWARM.sh
 
 # Write one entry per subject in Swarm file
 for i in `seq 0 1 ${num_subjects}`
--- Scripts/SC07_AdaptAtlasToSubject.sh
+++ Scripts/SC07_AdaptAtlasToSubject.sh
@@ -1,20 +1,20 @@
 set -e
 
-PRJDIR='/data/SFIM_Vigilance/PRJ_Vigilance_Smk01/'
+PRJDIR='/data/SFIM_Vigilance/PRJ_Vigilance_Smk02/'
 NROIS_ID=`printf %04d ${NROIS}` 
 ATLASID=`echo Craddock_T2Level_${NROIS_ID}`
 cd ${PRJDIR}/PrcsData/${SBJ}/D02_Preproc_fMRI
 
 # Here I use the mask for 60s becuase it is the most restrictive (less filtering --> more variance available)
 3dcalc -overwrite \
-       -a ${PRJDIR}/PrcsData/all/all.${ATLASID}.MNI+tlrc  \
-       -b ${PRJDIR}/PrcsData/${SBJ}/D02_Preproc_fMRI/full_mask.lowSigma.${SBJ}.wl060s+tlrc                                \
-       -expr 'a*b'                       \
+       -a ${PRJDIR}/PrcsData/all/all.${ATLASID}.MNI+tlrc \
+       -b ${PRJDIR}/PrcsData/${SBJ}/D02_Preproc_fMRI/full_mask.lowSigma.${SBJ}.wl060s+tlrc \
+       -expr 'a*b' \
        -prefix ${SBJ}.${ATLASID}.lowSigma
 
 # Give all the ROIs the lowest possible value
-3dmerge -overwrite -1rank                                    \
-        -prefix ${SBJ}.${ATLASID}.lowSigma        \
+3dmerge -overwrite -1rank \
+        -prefix ${SBJ}.${ATLASID}.lowSigma \
                 ${SBJ}.${ATLASID}.lowSigma+tlrc
 
 # Create a new dataset that contains the following:
@@ -30,6 +30,7 @@ do
  iID=`printf %03d ${i}`
  3dcalc -a ${SBJ}.${ATLASID}.lowSigma+tlrc -expr "equals(a,${i})" -overwrite -prefix ${SBJ}.${ATLASID}.lowSigma.${iID}
 done
+
 FILES=`ls ${SBJ}.${ATLASID}.lowSigma.???+tlrc.HEAD | tr -s '\n' ' ' | sed 's/.HEAD//'`
 echo ${FILES}
 3dbucket -overwrite -prefix ${SBJ}.${ATLASID}.lowSigma ${SBJ}.${ATLASID}.lowSigma+tlrc ${FILES}
