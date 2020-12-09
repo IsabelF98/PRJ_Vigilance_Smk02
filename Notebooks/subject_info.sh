bd9bb4f6b784db36fb36080373745ce5b6583d4e
Isabel Fernandez <izyf98@gmail.com>
2020-12-09 15:52:31 -0500

    changed text file output to output subject run and num of time points per run

--- Notebooks/subject_info.sh
+++ Notebooks/subject_info.sh
@@ -7,36 +7,37 @@ subjects=(`ls ${ORIG_DATA_DIR} | tr -s '\n' ' '`)
 subjects=("${subjects[@]/'README'}") # The subject directory contains a README file. This is not a subject ID.
 subjects=("${subjects[@]/'dataset_description.json'}") # The subject directory contains a json file. This is not a subject ID.
 subjects=("${subjects[@]/'sub-S21'}") # This subject had bad motion and will not be used.
+num_subjects=`echo "${#subjects[@]} -1" | bc -l`
+echo ${subjects[@]}
 
 cd ${PRJ_DIR}/Notebooks
 rm subject_run.txt
-rm subject_time.txt
+#rm subject_time.txt
 
 data_types=(SleepAscending SleepDescending SleepRSER WakeAscending WakeDescending WakeRSER)
+num_datatypes=`echo "${#data_types[@]} -1" | bc -l`
 
-for SBJ in ${subjects[@]}
+for i in `seq 0 1 ${num_subjects}`
 do
-    if [ ${SBJ} = 'sub-S12' ]
+    runs=()
+    run_times=(`3dinfo -nt ${PRJ_DIR}/PrcsData/${subjects[i]}/D02_Preproc_fMRI/pb05.${subjects[i]}.r0?.scale+tlrc.HEAD | awk '{print $1}' | tr -s '\n' ' '`)
+    if [ ${subjects[i]} = 'sub-S12' ]
     then
-        echo "${SBJ} SleepAscending" >> ./subject_run.txt
-        echo "${SBJ} SleepDescending" >> ./subject_run.txt
-        echo "${SBJ} SleepRSER" >> ./subject_run.txt
+        echo "${subjects[i]} SleepAscending 412" >> ./subject_run.txt
+        echo "${subjects[i]} SleepDescending 412" >> ./subject_run.txt
+        echo "${subjects[i]} SleepRSER 295" >> ./subject_run.txt
     else
-        for RUN in ${data_types[@]}
+        for n in `seq 0 1 ${num_datatypes}`
         do
-            if [ -f "${ORIG_DATA_DIR}/${SBJ}/ses-1/func/${SBJ}_ses-1_task-${RUN}_bold.nii" ]
+            if [ -f "${ORIG_DATA_DIR}/${subjects[i]}/ses-1/func/${subjects[i]}_ses-1_task-${data_types[n]}_bold.nii" ]
             then
-                echo "${SBJ} ${RUN}" >> ./subject_run.txt
+                runs+=(${data_types[n]})
             fi
+        done
+        num_runs=`echo "${#runs[@]} -1" | bc -l`
+        for n in `seq 0 1 ${num_runs}`
+            do
+                echo "${subjects[i]} ${runs[n]} ${run_times[n]}" >> ./subject_run.txt
         done
      fi
 done
-
-for SBJ in ${subjects[@]}
-do
-    run_times=(`3dinfo -nt ${PRJ_DIR}/PrcsData/${SBJ}/D02_Preproc_fMRI/pb05.${SBJ}.r0?.scale+tlrc.HEAD | awk '{print $1}' | tr -s '\n' ' '`)
-    for RT in ${run_times[@]}
-    do
-        echo "${SBJ} ${RT}" >> ./subject_time.txt
-    done
-done        
