803711fea96cbf61188cb4fa92342e934600bd1e
Isabel Fernandez <izyf98@gmail.com>
2021-01-05 16:20:16 -0500

    Created new notebook to plot distribution of sleep stages per subject

---
+++ Notebooks/Sleep_Staging_QA.py
@@ -0,0 +1,101 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.7.1
+#   kernelspec:
+#     display_name: Vigilance
+#     language: python
+#     name: vigilance
+# ---
+
+# # QA of Sleep Staging Data
+
+import pickle
+import os
+import os.path as osp
+import pandas as pd
+import numpy as np
+import hvplot.pandas
+import holoviews as hv
+import panel as pn
+from holoviews import dim, opts
+hv.extension('bokeh')
+
+# +
+PRJDIR   = '/data/SFIM_Vigilance/PRJ_Vigilance_Smk02/'
+sub_DF   = pd.read_pickle(PRJDIR+'Notebooks/utils/valid_run_df.pkl')
+sub_list = []
+for i,idx in enumerate(sub_DF.index): # Iterate through each row of data frame
+    sbj  = sub_DF.loc[idx]['Sbj']
+    if sbj in sub_list:
+        sbj = sbj
+    else:
+        sub_list.append(sbj)
+        
+SubjSelect = pn.widgets.Select(name='Select Subject', options=sub_list, value=sub_list[0])
+
+
+# -
+
+@pn.depends(SubjSelect.param.value)
+def sleep_bar_graph(SBJ):
+    PRJDIR   = '/data/SFIM_Vigilance/PRJ_Vigilance_Smk02/'
+
+    sleep_df = pd.DataFrame(columns=['sleep_value','sleep_stage'])
+    for RUN in ['SleepAscending','SleepDescending','SleepRSER']:
+        DATADIR  = osp.join(PRJDIR,'PrcsData',SBJ,'D02_Preproc_fMRI',SBJ+'_'+RUN+'_EEG_sleep.pkl')
+        if osp.exists(DATADIR) == True:
+            DATA_df  = pd.read_pickle(DATADIR)
+            temp_df  = pd.DataFrame(columns=['sleep_value','sleep_stage'])
+            temp_df['sleep_value'] = DATA_df['sleep']
+            sleep_df = sleep_df.append(temp_df).reset_index(drop = True)
+    for i,idx in enumerate(sleep_df.index):
+        if sleep_df.loc[idx, 'sleep_value'] == 0:
+            sleep_df.loc[idx, 'sleep_stage'] = 'Wake'
+        elif sleep_df.loc[idx, 'sleep_value'] == 1:
+            sleep_df.loc[idx, 'sleep_stage'] = 'Stage 1'
+        elif sleep_df.loc[idx, 'sleep_value'] == 2:
+            sleep_df.loc[idx, 'sleep_stage'] = 'Stage 2'
+        elif sleep_df.loc[idx, 'sleep_value'] == 3:
+            sleep_df.loc[idx, 'sleep_stage'] = 'Stage 3'
+        else:
+            sleep_df.loc[idx, 'sleep_stage'] = 'Undetermined'
+        
+    wake_df = pd.DataFrame(columns=['sleep_value','sleep_stage'])
+    for RUN in ['WakeAscending','WakeDescending','WakeRSER']:
+        DATADIR  = osp.join(PRJDIR,'PrcsData',SBJ,'D02_Preproc_fMRI',SBJ+'_'+RUN+'_EEG_sleep.pkl')
+        if osp.exists(DATADIR) == True:
+            DATA_df  = pd.read_pickle(DATADIR)
+            temp_df  = pd.DataFrame(columns=['sleep_value','sleep_stage'])
+            temp_df['sleep_value'] = DATA_df['sleep']
+            wake_df = wake_df.append(temp_df).reset_index(drop = True)
+    for i,idx in enumerate(wake_df.index):
+        if wake_df.loc[idx, 'sleep_value'] == 0:
+            wake_df.loc[idx, 'sleep_stage'] = 'Wake'
+        elif wake_df.loc[idx, 'sleep_value'] == 1:
+            wake_df.loc[idx, 'sleep_stage'] = 'Stage 1'
+        elif wake_df.loc[idx, 'sleep_value'] == 2:
+            wake_df.loc[idx, 'sleep_stage'] = 'Stage 2'
+        elif wake_df.loc[idx, 'sleep_value'] == 3:
+            wake_df.loc[idx, 'sleep_stage'] = 'Stage 3'
+        else:
+            wake_df.loc[idx, 'sleep_stage'] = 'Undetermined'
+            
+    count_df = pd.DataFrame(columns=['Run','Sleep_Stage','Count'])
+    sleep_list = list(sleep_df['sleep_stage'])
+    for stage in ['Wake','Stage 1','Stage 2','Stage 3', 'Undetermined']:
+        count_df.loc[len(count_df.index)] = ['Sleep',stage,sleep_list.count(stage)]
+    wake_list = list(wake_df['sleep_stage'])
+    for stage in ['Wake','Stage 1','Stage 2','Stage 3', 'Undetermined']:
+        count_df.loc[len(count_df.index)] = ['Wake',stage,wake_list.count(stage)]
+        
+    output = hv.Bars(count_df,kdims=['Run','Sleep_Stage']).opts(width=800,height=350,title='Sleep Stage Bar Graph for '+SBJ)
+    return output
+
+
+pn.Column(SubjSelect,sleep_bar_graph)
