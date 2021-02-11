98637915ded8c57ce008cc895ac4b107b61f70a4
Isabel Fernandez <izyf98@gmail.com>
2021-02-11 16:02:57 -0500

    eddited code for updated sleep staging data (starting at 6th TR)

--- Notebooks/N00b_load_EEG_sleep.ToSwarm.py
+++ Notebooks/N00b_load_EEG_sleep.ToSwarm.py
@@ -6,7 +6,7 @@ import os.path as osp
 
 DATADIR = '/data/SFIM_Vigilance/Data/' # Path to data directory
 PRJDIR = '/data/SFIM_Vigilance/PRJ_Vigilance_Smk02/' # Path to project directory
-EEG_csv_file = 'all_subs_all_EEG_metrics_continuous_epochs_0_4_12-13-2020.csv' # EEG file of sleep staging data
+EEG_csv_file = 'all_subs_all_EEG_metrics_continuous_epochs_0_4_02-08-2021.csv' # EEG file of sleep staging data
 
 EEG_sleep_df = pd.read_csv(DATADIR+EEG_csv_file,sep=',') # Load data frame of original EEG sleep staging data
 
@@ -24,6 +24,7 @@ for i,idx in enumerate(sub_DF.index):
 SBJ_1 = sys.argv[1] # Subject number in form 'sub-S??'
 SBJ_2 = SBJ_1.replace("-S","") # Subject number in form 'sub??'
 RUN   = sys.argv[2] # Run name
+
 out_path = osp.join(PRJDIR,'PrcsData',SBJ_1,'D02_Preproc_fMRI',SBJ_1+'_'+RUN+'_EEG_sleep.pkl') # File name of data being saved
 
 # Empty data frame of subject spacific EEG sleep staging data
@@ -40,14 +41,7 @@ max_TR = int(EEG_sbj_sleep_df.loc[int(num_TR)-1]['TR']) # Minimum number TR in d
 min_TR = int(EEG_sbj_sleep_df.loc[0]['TR']) # Maximum number TR in data
 TIME = [SubDict[SBJ_1][i][1] for i in range(0,len(SubDict[SBJ_1])) if SubDict[SBJ_1][i][0] == RUN][0] # Total number of TR's for run
 
-# Fill in missing TR data above
-temp_top = pd.DataFrame(columns=['dataset','subject','cond','TR','sleep','drowsiness','spectral','seconds'],index=range(6,min_TR))
-temp_top['dataset'] = EEG_sbj_sleep_df['dataset']
-temp_top['subject'] = EEG_sbj_sleep_df['subject']
-temp_top['cond']    = EEG_sbj_sleep_df['cond']
-temp_top['TR']      = range(6,min_TR)
-
-# Fill in missing TR data bellow
+# Fill in missing TRs in data bellow
 if max_TR != TIME+6:
     temp_bot = pd.DataFrame(columns=['dataset','subject','cond','TR','sleep','drowsiness','spectral','seconds'],index=range(0,(TIME+6)-(max_TR+1)))
     temp_bot['dataset'] = EEG_sbj_sleep_df['dataset']
@@ -56,6 +50,6 @@ if max_TR != TIME+6:
     temp_bot['TR']      = range(max_TR+1,TIME+6)
 
 # Concatinate data so all TR's are acounted for in data frame
-EEG_sbj_sleep_df = pd.concat([temp_top,EEG_sbj_sleep_df,temp_bot]).reset_index(drop = True)
+EEG_sbj_sleep_df = pd.concat([EEG_sbj_sleep_df,temp_bot]).reset_index(drop = True)
     
 EEG_sbj_sleep_df.to_pickle(out_path) # Save data frame as pickle file in subject directory
